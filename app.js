eca8ee7277407b076771486415fc09a02a8ced19
Adriana Molina <amolinalopez05@gmail.com>
2022-06-29 21:40:51 +0200

    error: 'invalid_client' peut etre voir le .env

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ const spotifyApi = new SpotifyWebApi({
 
 // Our routes go here:
 
-app.get('/', (req, res, next)=> {
-   res.render('homePage')
+app.get('/', (req, res, next) => {
+   res.render('homePage');
 })
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
